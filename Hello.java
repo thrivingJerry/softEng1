b4afa8a615bd20aa7cab944a437f7c27fe45b34a
thrivingJerry <142380693+thrivingJerry@users.noreply.github.com>
2023-08-16 14:26:07 +0800

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello{
+  public static void main(String[] args){
+    System.out.println("Hello, Software Engineering 1!");
+    System.out.println("Student name:  ESPERANZA, Jeremias");
+  }
+}
